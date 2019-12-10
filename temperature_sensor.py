121b9deaf14aaae62c8fc5285cb624af15a9e025
slimados <57549932+slimados@users.noreply.github.com>
2019-12-09 21:33:32 -0500

    Delete temperature_sensor.py

--- temperature_sensor.py
+++
@@ -1,9 +0,0 @@
-import time
-from w1thermsensor import W1ThermSensor
-sensor = W1ThermSensor()
-
-while True:
-    temperature = sensor.get_temperature()
-    print("Current Temperature is %s Celsius" % temperature)
-    time.sleep(1)
-        
